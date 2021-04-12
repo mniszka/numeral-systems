fe180910da5deac5b89c5a57cbf8f9dd98cdf59d
Agnieszka Fliśnik <aflisnik@wp.pl>
2021-04-12 14:57:08 +0200

    Update converting decimal to binary.cpp

--- converting decimal to binary.cpp
+++ converting decimal to binary.cpp
@@ -1,1 +1,34 @@
+//recursion
 
+
+#include <iostream>
+using namespace std;
+
+void convert(int number)
+{
+        if(number>0)
+        {
+              //when we use recursion, digit numbers will be display in inverse order
+
+                convert(number/2); 
+                cout<<number%2; //digit number     
+        }       
+}
+
+int main()
+{
+        int number;
+
+        for(;;)
+        {
+
+        cout<<"Put a decimal number bigger than 0: ";
+        cin>>number;
+        cout<<"Binar: ";
+        convert(number);
+        cout<<endl;
+          
+        };
+  
+        return 0;
+}
